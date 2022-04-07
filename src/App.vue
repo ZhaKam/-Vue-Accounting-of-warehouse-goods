faff648f3ea3ca91d0f6f15af29a39661c92d6f2
Zhakupova <puffy.eyes@mail.ru>
2022-04-07 10:00:38 +0600

    Rendering in a loop. The V-FOR Directive

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="product" v-for="product in products">
+      <div><strong>Название: </strong>{{ product.title }}</div>
+      <div><strong>Описание: </strong>{{ product.body }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      products: [
+        { id: 1, title: "DEMO1", body: "Черновое описание" },
+        { id: 2, title: "DEMO2", body: "Черновое описание" },
+        { id: 3, title: "DEMO3", body: "Черновое описание" },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.product {
+  padding: 15px;
+  border: 2px solid mediumslateblue;
+  margin-top: 15px;
+}
+</style>
